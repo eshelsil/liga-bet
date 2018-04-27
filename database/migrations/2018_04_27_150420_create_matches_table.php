d355acac599a3aa3171042d45306d99bdae190b6
Kima <omer@paymeservice.com>
2018-04-27 19:01:48 +0300

    First push

---
+++ database/migrations/2018_04_27_150420_create_matches_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("external_id")->nullable();
+            $table->integer("team_home_id");
+            $table->integer("team_out_id");
+            $table->integer("game_start_time")->nullable();
+            $table->integer("score_home")->nullable();
+            $table->integer("score_out")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+}
---
+++ database/migrations/2018_04_27_150518_create_bets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("match_id");
+            $table->integer("user_id");
+            $table->text("data");
+            $table->integer("score")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bets');
+    }
+}
