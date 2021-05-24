49653a50c1ee031b9206e0f874ebfd60a4324c51
EshelSil <eshelsil@gmail.com>
2021-05-24 17:29:58 +0300

    all-group-bets-view - add option to see group-rank-bets by bet-value

--- resources/views/all-group-bets-view.blade.php
+++ resources/views/all-group-bets-view.blade.php
@@ -22,34 +22,117 @@
             </div>
         </div>
         <div id="collapserank-{{$group->id}}" class="panel-collapse collapse">
-            <ul style="margin-left: 20px;">
-                <li class="list-group-item row" style="background: #d2d2d2;">
-                    <div class="col-sm-4 pull-right">שם</div>
-                    <div class="col-sm-7 pull-right">הימור</div>
-                </li>
-                @foreach($usersWhoBet as $user)
-                @php    
-                    $bet = $user->bets->where('type_id', $group->id)->first();
-                @endphp
-                @if ($bet)
-                    <li class="list-group-item row">
-                        <div class="col-sm-4 pull-right">{{ $user->name }}</div>
-                        <div class="col-sm-7 pull-right">
-                        @foreach (range(1,4) as $position)
-                            @php
-                                $bet_team_id = $bet->getData($position);
-                                $bet_team = $group->teams->find($bet_team_id);
-                            @endphp
-                            <div class="flex-row">
-                                <span>({{$position}}) </span>
-                                @include('widgets.teamWithFlag', $bet_team)
-                            </div>
-                        @endforeach
-                        </div>
+            
+
+            <div class="float-right">
+                <ul class="nav nav-tabs float-right">
+                    <li class="float-right active">
+                        <a data-toggle="tab" href="#by-bet-{{$group->id}}">
+                            לפי הימור
+                        </a>
                     </li>
-                @endif
-                @endforeach
-            </ul>        
+                    <li class="float-right">
+                        <a data-toggle="tab" href="#by-user-{{$group->id}}">
+                            לפי משתמש
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="tab-content" style="margin-top: 25px">
+                <div id="by-bet-{{$group->id}}" class="tab-pane fade active in" style="padding: 20px; margin-bottom: 30px;">
+                        <?php
+                            $bettersByBetValue = [];
+                            foreach($usersWhoBet as $user){
+                                $bet = $user->bets->where('type_id', $group->id)->first();
+                                if (!$bet){
+                                    continue;
+                                }
+                                $bet_data = $bet->getData();
+                                $bet_value = json_encode($bet_data);
+                                if (!array_key_exists($bet_value, $bettersByBetValue)){
+                                    $bettersByBetValue[$bet_value] = [];
+                                }
+                                $bettersByBetValue[$bet_value][] = $user->name;
+                                $bettersByBetValue = collect($bettersByBetValue)->sort(function ($a, $b) {
+                                    return count($b) - count($a);
+                                })->toArray();
+                            }
+                        ?>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th class="col-sm-4">
+                                    הימור
+                                </th>
+                                <th class="col-sm-8">
+                                    מהמרים
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($bettersByBetValue as $betValue => $names)
+                                <?php    
+                                    $bet_data = json_decode($betValue);
+                                ?>
+                                <tr>
+                                    <td>
+                                    @foreach (range(1,4) as $position)
+                                        <?php
+                                            $bet_team_id = data_get($bet_data, $position);
+                                            $bet_team = $group->teams->find($bet_team_id);
+                                        ?>
+                                        <div class="flex-row">
+                                            <span>({{$position}}) </span>
+                                            @include('widgets.teamWithFlag', $bet_team)
+                                        </div>
+                                    @endforeach
+                                    </td>
+                                    <td>{!! implode('<br>', $names) !!}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <div id="by-user-{{$group->id}}" class="tab-pane fade" style="padding: 20px; margin-bottom: 30px;">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th class="col-sm-4">
+                                    שם
+                                </th>
+                                <th class="col-sm-8">
+                                    הימור
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($usersWhoBet as $user)
+                                <?php    
+                                    $bet = $user->bets->where('type_id', $group->id)->first();
+                                ?>
+                                @if ($bet)
+                                    <tr>
+                                        <td>{{ $user->name }}</td>
+                                        <td>
+                                        @foreach (range(1,4) as $position)
+                                            <?php
+                                                $bet_team_id = $bet->getData($position);
+                                                $bet_team = $group->teams->find($bet_team_id);
+                                            ?>
+                                            <div class="flex-row">
+                                                <span>({{$position}}) </span>
+                                                @include('widgets.teamWithFlag', $bet_team)
+                                            </div>
+                                        @endforeach
+                                        </td>
+                                    </tr>
+                                @endif
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
