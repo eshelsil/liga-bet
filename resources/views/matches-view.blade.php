7abbac20821e3a5cb55df45b8811a10efd1c8a4f
EshelSil <eshelsil@gmail.com>
2021-05-24 16:20:01 +0300

    matches-view - add option to view user bets groupd by bet-value

--- resources/views/matches-view.blade.php
+++ resources/views/matches-view.blade.php
@@ -29,23 +29,73 @@
             </table>
         </h3>
 
-        <table class="table" style="margin-bottom: 70px;">
-            <thead>
-            <tr>
-                <td class="admin">מזהה מוכר</td>
-                <th>שם</th>
-                <th>תוצאה</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($match->getBets() as $bet)
-                <tr>
-                    <td class="admin">{{$bet->user->id}}</td>
-                    <td>{{$bet->user->name}}</td>
-                    <td>{{$bet->getData("result-away")}}:{{$bet->getData("result-home")}}</td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
+        <div class="float-right">
+            <ul class="nav nav-tabs float-right">
+                <li class="float-right active">
+                    <a data-toggle="tab" href="#by-bet-{{$match->getID()}}">
+                        לפי הימור
+                    </a>
+                </li>
+                <li class="float-right">
+                    <a data-toggle="tab" href="#by-user-{{$match->getID()}}">
+                        לפי משתמש
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="tab-content" style="margin-top: 25px; margin-bottom: 70px;">
+            <div id="by-bet-{{$match->getID()}}" class="tab-pane fade active in" style="padding: 20px;">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <td class="admin">מזהה מוכר</td>
+                            <th class="col-sm-6">תוצאה</th>
+                            <th class="col-sm-6">מהמרים</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                        $bettersByBetValue = [];
+                        foreach($match->getBets() as $bet){
+                            $betVal = "".$bet->getData("result-away").":".$bet->getData("result-home");
+                            if (!array_key_exists($betVal, $bettersByBetValue)){
+                                $bettersByBetValue[$betVal] = [];
+                            }
+                            $bettersByBetValue[$betVal][] = $bet->user->name;
+                        }
+                    ?>
+                    @foreach($bettersByBetValue as $betVal => $names)
+                        <tr>
+                            <td class="admin">{{$bet->user->id}}</td>
+                            <td>{{$betVal}}</td>
+                            <td>{!! implode('<br>', $names) !!}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div id="by-user-{{$match->getID()}}" class="tab-pane fade" style="padding: 20px;">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <td class="admin">מזהה מוכר</td>
+                            <th class="col-sm-6">שם</th>
+                            <th class="col-sm-6">תוצאה</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($match->getBets() as $bet)
+                        <tr>
+                            <td class="admin">{{$bet->user->id}}</td>
+                            <td>{{$bet->user->name}}</td>
+                            <td>{{$bet->getData("result-away")}}:{{$bet->getData("result-home")}}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        
     @endforeach
 @endsection
