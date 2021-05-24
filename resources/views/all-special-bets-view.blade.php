a96ca5d76df5152030a5f2fe508ef4f002acd41e
EshelSil <eshelsil@gmail.com>
2021-05-24 16:47:00 +0300

    all-special-bets-view  - Show add option to show bets by user's name

--- resources/views/all-special-bets-view.blade.php
+++ resources/views/all-special-bets-view.blade.php
@@ -22,39 +22,82 @@
                 ?>
                 <div id="special-bet-wrapper-{{$betId}}" class="tab-pane fade {{$index == 0 ? 'active in' : ''}}" style="padding: 20px;">
                     <h3 class="text-center">{{$betTitle}}</h3>
-                    <ul class="list-group">
-                        <li class="list-group-item row" style="background: #d2d2d2;">
-                            <div class="col-sm-5 pull-right">הימור</div>
-                            <div class="col-sm-5 pull-right">מהמרים</div>
-                        </li>
-                        <?php
-                            $relevantBets = $bets->where('type_id', $betId);
-                            $usersByBetValue = [];
-                            foreach($relevantBets as $bet){
-                                $answer = $bet->getAnswer();
-                                if (!array_key_exists($answer, $usersByBetValue)){
-                                    $usersByBetValue[$answer] = [];
-                                }
-                                array_push($usersByBetValue[$answer], $bet->user);
-                            }
-                            $usersByBetValue = collect($usersByBetValue)->sort(function ($a, $b) {
-                                return count($b) - count($a);
-                            })->toArray();
-                        ?>
-                        @foreach ($usersByBetValue as $betValue => $users)                        
-                            <?php
-                                $betters = array_map(function($u){
-                                    return $u->name;
-                                }, $users);
-                                $bettersDescription = implode('<br>', $betters);
-                            ?>
-                            <li class="list-group-item row">
-                                <div class="col-sm-5 pull-right">{!! $specialBet->formatDescription($betValue) !!}</div>
-                                <div class="col-sm-5 pull-right">{!! $bettersDescription !!}</div>
+                    <div class="float-right">
+                        <ul class="nav nav-tabs float-right">
+                            <li class="float-right active">
+                                <a data-toggle="tab" href="#by-bet-{{$betId}}">
+                                    לפי הימור
+                                </a>
                             </li>
-                        @endforeach
+                            <li class="float-right">
+                                <a data-toggle="tab" href="#by-user-{{$betId}}">
+                                    לפי משתמש
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="tab-content" style="margin-top: 25px;">
+                        <div id="by-bet-{{$betId}}" class="tab-pane fade active in" style="padding: 20px; padding-top: 35px;">
+                            <ul class="list-group">
+                                <li class="list-group-item row" style="background: #d2d2d2;">
+                                    <div class="col-sm-5 pull-right">הימור</div>
+                                    <div class="col-sm-5 pull-right">מהמרים</div>
+                                </li>
+                                <?php
+                                    $relevantBets = $bets->where('type_id', $betId);
+                                    $usersByBetValue = [];
+                                    foreach($relevantBets as $bet){
+                                        $answer = $bet->getAnswer();
+                                        if (!array_key_exists($answer, $usersByBetValue)){
+                                            $usersByBetValue[$answer] = [];
+                                        }
+                                        array_push($usersByBetValue[$answer], $bet->user);
+                                    }
+                                    $usersByBetValue = collect($usersByBetValue)->sort(function ($a, $b) {
+                                        return count($b) - count($a);
+                                    })->toArray();
+                                ?>
+                                @foreach ($usersByBetValue as $betValue => $users)                        
+                                    <?php
+                                        $betters = array_map(function($u){
+                                            return $u->name;
+                                        }, $users);
+                                        $bettersDescription = implode('<br>', $betters);
+                                    ?>
+                                    <li class="list-group-item row">
+                                        <div class="col-sm-5 pull-right">{!! $specialBet->formatDescription($betValue) !!}</div>
+                                        <div class="col-sm-5 pull-right">{!! $bettersDescription !!}</div>
+                                    </li>
+                                @endforeach
+        
+                            </ul>
+                        </div>
+                        <div id="by-user-{{$betId}}" class="tab-pane fade" style="padding: 20px; padding-top: 35px;">
+                            <?php
+                                    $usersWithBets = [];
+                                    foreach($usersByBetValue as $betValue => $users){
+                                        foreach ($users as $user) {
+                                            $user->betValue = $betValue;
+                                            $usersWithBets[] = $user;
+                                        }
+                                    }
+                                ?>
+                            <ul class="list-group">
+                                <li class="list-group-item row" style="background: #d2d2d2;">
+                                    <div class="col-sm-5 pull-right">שם</div>
+                                    <div class="col-sm-5 pull-right">הימור</div>
+                                </li>
+                                @foreach($usersWithBets as $user)
+                                <li class="list-group-item row">
+                                    <div class="col-sm-5 pull-right">{{$user->name}}</div>
+                                    <div class="col-sm-5 pull-right">{!! $specialBet->formatDescription($user->betValue) !!}</div>
+                                </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
 
-                    </ul>
+                    
                 </div>
                 @endforeach
             </div>
