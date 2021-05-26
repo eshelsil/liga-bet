32bdf519f2ba216c644a39f97adac55e38133f76
EshelSil <eshelsil@gmail.com>
2021-05-26 03:17:28 +0300

    all-special-bets - support mobile

--- resources/views/all-special-bets-view.blade.php
+++ resources/views/all-special-bets-view.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.home')
 
 @section('content')
-    <div class="row float-right">
+    <div class="row">
         <h1>הימורים לטווח רחוק</h1>
         <div class="float-right">
-            <ul class="nav nav-tabs float-right">
+            <ul class="nav nav-tabs" style="padding-right: 0px;">
                 @foreach($specialBets as $index => $specialBet)
                     <li class="float-right {{$index == 0 ? 'active' : ''}}">
                     <a data-toggle="tab" href="#special-bet-wrapper-{{$specialBet->getID()}}">
@@ -20,10 +20,10 @@
                     $betId = $specialBet->getID();
                     $betTitle = $specialBet->getTitle();
                 ?>
-                <div id="special-bet-wrapper-{{$betId}}" class="tab-pane fade {{$index == 0 ? 'active in' : ''}}" style="padding: 20px;">
+                <div id="special-bet-wrapper-{{$betId}}" class="tab-pane fade {{$index == 0 ? 'active in' : ''}}" style="padding: 10px;">
                     <h3 class="text-center">{{$betTitle}}</h3>
                     <div class="float-right">
-                        <ul class="nav nav-tabs float-right">
+                        <ul class="nav nav-tabs float-right"  style="padding-right: 0px;">
                             <li class="float-right active">
                                 <a data-toggle="tab" href="#by-bet-{{$betId}}">
                                     לפי הימור
@@ -37,9 +37,9 @@
                         </ul>
                     </div>
                     <div class="tab-content" style="margin-top: 25px;">
-                        <div id="by-bet-{{$betId}}" class="tab-pane fade active in" style="padding: 20px; padding-top: 35px;">
-                            <ul class="list-group">
-                                <li class="list-group-item row" style="background: #d2d2d2;">
+                        <div id="by-bet-{{$betId}}" class="tab-pane fade active in" style="padding-top: 35px;">
+                            <ul class="list-group" style="padding-right: 0px;">
+                                <li class="list-group-item row full-row" style="background: #d2d2d2;">
                                     <div class="col-xs-5 pull-right">הימור</div>
                                     <div class="col-xs-5 pull-right">מהמרים</div>
                                 </li>
@@ -64,7 +64,7 @@
                                         }, $users);
                                         $bettersDescription = implode('<br>', $betters);
                                     ?>
-                                    <li class="list-group-item row">
+                                    <li class="list-group-item row full-row">
                                         <div class="col-xs-5 pull-right">{!! $specialBet->formatDescription($betValue) !!}</div>
                                         <div class="col-xs-5 pull-right">{!! $bettersDescription !!}</div>
                                     </li>
@@ -82,13 +82,13 @@
                                         }
                                     }
                                 ?>
-                            <ul class="list-group">
-                                <li class="list-group-item row" style="background: #d2d2d2;">
+                            <ul class="list-group" style="padding-right: 0px;">
+                                <li class="list-group-item row full-row" style="background: #d2d2d2;">
                                     <div class="col-xs-5 pull-right">שם</div>
                                     <div class="col-xs-5 pull-right">הימור</div>
                                 </li>
                                 @foreach($usersWithBets as $user)
-                                <li class="list-group-item row">
+                                <li class="list-group-item row full-row">
                                     <div class="col-xs-5 pull-right">{{$user->name}}</div>
                                     <div class="col-xs-5 pull-right">{!! $specialBet->formatDescription($user->betValue) !!}</div>
                                 </li>
