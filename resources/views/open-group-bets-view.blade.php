7da04fbf2b15561cafe51f79845d8f2271f46abb
EshelSil <eshelsil@gmail.com>
2021-05-26 03:16:06 +0300

    open-group-bets-view - support mobile

--- resources/views/open-group-bets-view.blade.php
+++ resources/views/open-group-bets-view.blade.php
@@ -72,74 +72,75 @@
 
 @section('content')
         @if(\App\Group::areBetsOpen())
-        <h2>הימורי בתים פתוחים</h2>
-        @foreach($groupsTeamsData as $i => $groupTeamsData)
-        @php
-            $teams = data_get($groupTeamsData, 'teams');
-            $group = $groupsByExternalId[data_get($groupTeamsData, 'group_id')];
-        @endphp
-        <div class="col-xs-12 col-md-9 col-lg-7" style="float: right; border-radius: 5px; border: #000 1px solid; margin-bottom: 25px; padding: 10px;">
-            <h5 style="text-align: center;">{{$group->name}}</h5>
-            <div class="row">
-                @php
-                $currentGroup = $currentBetsById[$group->id];
-                $currentBet = $currentGroup->bet;
-                @endphp
-                @if($currentBet)
+        <div class="row">
+            <h2>הימורי בתים פתוחים</h2>
+            @foreach($groupsTeamsData as $i => $groupTeamsData)
+            @php
+                $teams = data_get($groupTeamsData, 'teams');
+                $group = $groupsByExternalId[data_get($groupTeamsData, 'group_id')];
+            @endphp
+            <div class="col-xs-12 col-md-9 col-lg-7" style="width: 80%; float: right; border-radius: 5px; border: #000 1px solid; margin-bottom: 25px; padding: 10px;">
+                <h5 style="text-align: center;">{{$group->name}}</h5>
+                <div class="row">
                     @php
-                        $teams_by_bet_order = [];
-                        foreach(json_decode($currentBet->data) as $position => $teamId){
-                            array_push($teams_by_bet_order, $teams->find($teamId));
-                        }
-                        $teams = $teams_by_bet_order;
+                    $currentGroup = $currentBetsById[$group->id];
+                    $currentBet = $currentGroup->bet;
                     @endphp
-                    <div id="current-bet-{{$group->id}}-position" class="col-xs-5" >
-                        <h6>הימור נוכחי:</h6>
-                        <ol class="currentBet">
-                        @foreach(json_decode($currentBet->data) as $position => $teamId)
-                            @php
-                            $teamData = $teamsById[$teamId];
-                            @endphp
-                            <li style="font-size: 80%;" data-pos="{{$position}}">
-                                @include('widgets.teamWithFlag', $teamData)
-                            </li>
-                        @endforeach
-                        </ol>
-                    </div>
-
-                @else
-                    <div id="current-bet-{{$group->id}}-position" class="col-xs-5" hidden>
-                        <h6>הימור נוכחי:</h6>
-                        <ol class="currentBet">
-                        @foreach(range(1,4) as $position)
-                            <li style="font-size: 80%;" data-pos="{{$position}}">
-                                @include('widgets.teamWithFlag')
-                            </li>
-                        @endforeach
-                        </ol>
-                    </div>
-
-                @endif
-                    <div id="set-bet-table-{{$group->id}}" class="{{$currentBet ? 'col-xs-7' : 'col-xs-12'}}">
-                    <ol class="sortable" data-group="{{$group->id}}">
-                        @foreach($teams as $team_data)
-                            <div class="team_row" data-team-id="{{$team_data->id}}"  onmousedown="inputChange({{$group->id}})">
-                                <li class="bg-info">
-                                    <img class="team_flag larger no-border" src="{{$team_data->crest_url}}">
-                                    <span style="position:relative;">{{$team_data->name}}</span>
+                    @if($currentBet)
+                        @php
+                            $teams_by_bet_order = [];
+                            foreach(json_decode($currentBet->data) as $position => $teamId){
+                                array_push($teams_by_bet_order, $teams->find($teamId));
+                            }
+                            $teams = $teams_by_bet_order;
+                        @endphp
+                        <div id="current-bet-{{$group->id}}-position" class="col-xs-5" >
+                            <h6>הימור נוכחי:</h6>
+                            <ol class="currentBet">
+                            @foreach(json_decode($currentBet->data) as $position => $teamId)
+                                @php
+                                $teamData = $teamsById[$teamId];
+                                @endphp
+                                <li style="font-size: 80%;" data-pos="{{$position}}">
+                                    @include('widgets.teamWithFlag', $teamData)
                                 </li>
-                            </div>
-                                @endforeach
-                    </ol>
-                    <div style="padding-right:40px;">
-                        <button id="save-bet-{{$group->id}}" onClick="sendBet('{{$group->id}}')" type="button" class="btn btn-primary">שלח</button>
+                            @endforeach
+                            </ol>
+                        </div>
+
+                    @else
+                        <div id="current-bet-{{$group->id}}-position" class="col-xs-5" hidden>
+                            <h6>הימור נוכחי:</h6>
+                            <ol class="currentBet">
+                            @foreach(range(1,4) as $position)
+                                <li style="font-size: 80%;" data-pos="{{$position}}">
+                                    @include('widgets.teamWithFlag')
+                                </li>
+                            @endforeach
+                            </ol>
+                        </div>
+
+                    @endif
+                        <div id="set-bet-table-{{$group->id}}" class="{{$currentBet ? 'col-xs-7' : 'col-xs-12'}}">
+                        <ol class="sortable" data-group="{{$group->id}}">
+                            @foreach($teams as $team_data)
+                                <div class="team_row" data-team-id="{{$team_data->id}}"  onmousedown="inputChange({{$group->id}})">
+                                    <li class="bg-info">
+                                        <img class="team_flag larger no-border" src="{{$team_data->crest_url}}">
+                                        <span style="position:relative;">{{$team_data->name}}</span>
+                                    </li>
+                                </div>
+                                    @endforeach
+                        </ol>
+                        <div style="padding-right:40px;">
+                            <button id="save-bet-{{$group->id}}" onClick="sendBet('{{$group->id}}')" type="button" class="btn btn-primary">שלח</button>
+                        </div>
                     </div>
                 </div>
             </div>
+            
+            @endforeach
         </div>
-        
-        @endforeach
-
         @else
         <h2>נסגרו הימורי הבתים! לא ניתן לעדכן הימורים אלה</h2>
         @endif
