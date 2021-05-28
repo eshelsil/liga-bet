d7701e0d2963c146bafb75ba75b23b7c95acbcf6
EshelSil <eshelsil@gmail.com>
2021-05-28 16:12:48 +0300

    add default top-scorer-players to tournamentData config

--- config/tournamentData.php
+++ config/tournamentData.php
@@ -3,5 +3,181 @@
 return [
     'groupStageGamesCount' => 6 * 6,
     'topScorerBets' => [
+        [
+            "id" => 311,
+            'name' => 'Serge Gnabry',
+            'team_ext_id' => 759
+        ],
+        [
+            'id' => 3187,
+            'name' => 'Timo Werner',
+            'team_ext_id' => 759
+        ],
+        [
+            'id' => 171,
+            'name' => 'Kai Havertz',
+            'team_ext_id' => 759
+        ],
+        [
+            'id' => 3184,
+            'name' => 'Leroy Sane',
+            'team_ext_id' => 759
+        ],
+        [
+            'id' => 370,
+            'name' => 'Thomas Muller',
+            'team_ext_id' => 759
+        ],
+        [
+            'id' => 3182,
+            'name' => 'Ilkay Gundogan',
+            'team_ext_id' => 759
+        ],
+        
+        [
+            'id' => 7819,
+            'name' => 'Alvaro Morata',
+            'team_ext_id' => 760
+        ],
+        [
+            'id' => 32719,
+            'name' => 'Gerard Moreno',
+            'team_ext_id' => 760
+        ],
+        [
+            'id' => 33154,
+            'name' => 'Ferran Torres',
+            'team_ext_id' => 760
+        ],
+        [
+            'id' => 44,
+            'name' => 'Cristiano Ronaldo',
+            'team_ext_id' => 765
+        ],
+        [
+            'id' => 1760,
+            'name' => 'Andre Silva',
+            'team_ext_id' => 765
+        ],
+        [
+            'id' => 3257,
+            'name' => 'Bruno Fernandes',
+            'team_ext_id' => 765
+        ],
+        [
+            'id' => 4092,
+            'name' => 'Diogo Jota',
+            'team_ext_id' => 765
+        ],
+        [
+            'id' => 8004,
+            'name' => 'Harry Kane',
+            'team_ext_id' => 770
+        ],
+        [
+            'id' => 3329,
+            'name' => 'Raheem Sterling',
+            'team_ext_id' => 770
+        ],
+        [
+            'id' => 3331,
+            'name' => 'Marcus Rashford',
+            'team_ext_id' => 770
+        ],
+        [
+            'id' => 7888,
+            'name' => 'Phil Foden',
+            'team_ext_id' => 770
+        ],
+        [
+            'id' => 3374,
+            'name' => 'Kylian Mbappe',
+            'team_ext_id' => 773
+        ],
+        [
+            'id' => 114,
+            'name' => 'Antoine Griezmann',
+            'team_ext_id' => 773
+        ],
+        [
+            'id' => 3371,
+            'name' => 'Olivier Giroud',
+            'team_ext_id' => 773
+        ],
+        [
+            'id' => 73,
+            'name' => 'Wissam Ben Yedder',
+            'team_ext_id' => 773
+        ],
+        [
+            'id' => 49,
+            'name' => 'Karim Benzema',
+            'team_ext_id' => 773
+        ],
+        [
+            'id' => 2076,
+            'name' => 'Ciro Immobile',
+            'team_ext_id' => 784
+        ],
+        [
+            'id' => 2298,
+            'name' => 'Andrea Belotti',
+            'team_ext_id' => 784
+        ],
+        [
+            'id' => 2092,
+            'name' => 'Lorenzo Insigne',
+            'team_ext_id' => 784
+        ],
+        [
+            'id' => 371,
+            'name' => 'Robert Lewandowski',
+            'team_ext_id' => 794
+        ],
+        [
+            'id' => 30323,
+            'name' => 'Burak Yilmaz',
+            'team_ext_id' => 803
+        ],
+        [
+            'id' => 3662,
+            'name' => 'Romelu Lukaku',
+            'team_ext_id' => 805
+        ],
+        [
+            'id' => 3653,
+            'name' => 'Eden Hazard',
+            'team_ext_id' => 805
+        ],
+        [
+            'id' => 3664,
+            'name' => 'Michy Batshuayi',
+            'team_ext_id' => 805
+        ],
+        [
+            'id' => 8834,
+            'name' => 'Artem Dzyuba',
+            'team_ext_id' => 808
+        ],
+        [
+            'id' => 3876,
+            'name' => 'Gareth Bale',
+            'team_ext_id' => 833
+        ],
+        [
+            'id' => 8472,
+            'name' => 'Memphis Depay',
+            'team_ext_id' => 8601
+        ],
+        [
+            'id' => 7871,
+            'name' => 'Georginio Wijnaldum',
+            'team_ext_id' => 8601
+        ],
+        [
+            'id' => 7457,
+            'name' => 'Donyell Malen',
+            'team_ext_id' => 8601
+        ],
     ],
 ];
