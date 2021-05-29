8c8202cd302bef5f1037d6160248824c3e57a4d3
EshelSil <eshelsil@gmail.com>
2021-05-29 13:03:11 +0300

    config.bets - set default value of 'lockBeforeSeconds', 'lockBetsBeforeTournamentSeconds' to 0

--- config/bets.php
+++ config/bets.php
@@ -2,8 +2,8 @@
 
 return [
 
-    'lockBeforeSeconds' => 60*60*env('BETS_LOCK_BET_BEFORE_HOURS', 3),
-    'lockBetsBeforeTournamentSeconds' => 60*60*env('BETS_LOCK_BETS_BEFORE_TOURNAMENT_HOURS', 22),
+    'lockBeforeSeconds' => 60*60*env('BETS_LOCK_BET_BEFORE_HOURS', 0),
+    'lockBetsBeforeTournamentSeconds' => 60*60*env('BETS_LOCK_BETS_BEFORE_TOURNAMENT_HOURS', 0),
     'mvp' => env('MVP', null),
     'topAssists' => env('TOP_ASSISTS_PLAYER', null),
 ];
