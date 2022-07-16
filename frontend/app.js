1a8efe88bb87979031181c817725237e35a5f0eb
EshelSil <eshelsil@gmail.com>
2022-07-16 13:43:16 +0300

    frontend/app/js - serve react app at '*'

--- frontend/app.js
+++ frontend/app.js
@@ -1,12 +1,13 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 4000
 
-app.use(express.static('../public'))
+app.use('/static', express.static('../public'))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
