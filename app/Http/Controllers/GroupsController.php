52b8d79416975ebcae35098cefe6097686917a33
kima <omer@paymeservice.com>
2022-07-28 01:09:50 +0300

    test

--- app/Http/Controllers/GroupsController.php
+++ app/Http/Controllers/GroupsController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\GroupResource;
 use App\Group;
+use App\TournamentUser;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class GroupsController extends Controller
 {
 
     public function index(Request $request, string $tournamentId)
     {
+        Log::warning("[GroupsController][index] Received request!");
         $utl = $this->getUser()->getTournamentUser($tournamentId);
 
         $utl->tournament->competition->groups->load("teams");
