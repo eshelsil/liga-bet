8e27689ccb27b8cc79c9c5d34618f72e3c6774bf
kima <omer@paymeservice.com>
2022-07-28 00:54:17 +0300

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,11 +9,6 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function __construct()
-    {
-       $this->middleware('auth');
-    }
-
     public function showSetPassword()
     {
         return view('set_password');
