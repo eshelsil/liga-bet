75ab30d6c9e3953d57b8703f509afef2f870868c
EshelSil <eshelsil@gmail.com>
2022-07-30 15:30:40 +0300

    FE: move index.js to typescript

---
+++ frontend/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom'; 
+// import "core-js/stable";
+// import "regenerator-runtime/runtime";
+import App from './App';
+  
+render(
+    <App/>
+  ,
+  document.getElementById('root')
+);
