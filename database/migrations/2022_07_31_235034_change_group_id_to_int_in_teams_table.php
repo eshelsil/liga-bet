d36adc78de082c296aa57f83877dd7b0f0ef06d6
kima <omer@paymeservice.com>
2022-07-31 23:57:12 +0300

    Fix group_id bug

--- database/migrations/2022_07_31_235034_change_group_id_to_int_in_teams_table.php
+++ database/migrations/2022_07_31_235034_change_group_id_to_int_in_teams_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class ChangeGroupIdToIntInTeamsTable extends Migration
@@ -13,9 +14,8 @@ class ChangeGroupIdToIntInTeamsTable extends Migration
      */
     public function up()
     {
-        Schema::table('teams', function (Blueprint $table) {
-            $table->integer("group_id")->change();
-        });
+        DB::statement('ALTER TABLE teams ALTER COLUMN 
+                  group_id TYPE integer USING (group_id)::integer');
     }
 
     /**
