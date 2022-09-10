4210ce96d7f72176f20738abaa443247e19aa138
EshelSil <eshelsil@gmail.com>
2022-09-11 02:24:12 +0300

    [FE] SearchBar - implement debounced change

--- frontend/src/widgets/SearchBar/SearchBar.tsx
+++ frontend/src/widgets/SearchBar/SearchBar.tsx
@@ -1,31 +1,47 @@
-import React from 'react';
+import React, { useMemo, useEffect, useState } from 'react';
 import { TextField, InputAdornment, IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
 import './SearchBar.scss';
+import { debounce } from 'lodash';
 
 
 
 interface Props {
-    value: string,
     onChange: (val: string) => void,
+    defaultValue?: string,
     placeholder?: string,
 }
 
 function SearchBar({
-    value,
     onChange,
+    defaultValue = '',
     placeholder = 'Search',
 }: Props){
+    const [value, setValue] = useState(defaultValue);
     const isEmpty = value.length === 0;
-    function onClear(){
-        onChange('');
+    const debouncedOnChange = useMemo(
+		() => debounce(onChange, 300),
+		[onChange]
+	);
+    function handleInputChange(newVal: string){
+        setValue(newVal);
+        debouncedOnChange(newVal);
     }
+    function onClear(){
+        handleInputChange('');
+    }
+
+    useEffect(()=>{
+		return () => {
+			debouncedOnChange.cancel()
+		}
+	}, [])
     return (
         <div className={'LigaBet-SearchBar'}>
             <TextField
-                value={value || ''}
-                onChange={e => onChange(e.target.value)}
+                value={value}
+                onChange={e => handleInputChange(e.target.value)}
                 placeholder={placeholder}
                 InputProps={{
                     endAdornment: (
