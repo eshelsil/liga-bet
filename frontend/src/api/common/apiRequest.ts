2127e487d12a090cbe6f464f6a4ae3c3fb1af17b
EshelSil <eshelsil@gmail.com>
2022-09-11 02:23:22 +0300

    [FE] sendApiRequest - support using response-headers

--- frontend/src/api/common/apiRequest.ts
+++ frontend/src/api/common/apiRequest.ts
@@ -4,22 +4,41 @@ interface ApiRequestParams {
     type?: string,
     url: string,
     data?: object,
+    includeResponseHeaders?: string[],
 }
 
 export const sendApiRequest = async ({
     type = 'GET',
     url,
     data,
-}: ApiRequestParams) => {
-    return await (window as any).$.ajax({
-        type,
-        url,
-        contentType: 'application/json',
-        dataType: 'json',
-        data: data ? JSON.stringify(data) : undefined,
-        error: function(data) {
-            console.error(data);
-            reportApiError(data);
-        }
+    includeResponseHeaders,
+}: ApiRequestParams): Promise<any> => {
+    return new Promise((resolve, reject) => {
+        (window as any).$.ajax({
+            type,
+            url,
+            contentType: 'application/json',
+            dataType: 'json',
+            data: data ? JSON.stringify(data) : undefined,
+            success: function(data: any, textStatus, request){
+                if (includeResponseHeaders){
+                    const headers = {} as Record<string, any>;
+                    for (const header of includeResponseHeaders){
+                        headers[header] = request.getResponseHeader(header);
+                    }
+                    resolve({
+                        data,
+                        headers,
+                    });
+                } else {
+                    resolve(data);
+                }
+            },
+            error: function(data) {
+                console.error(data);
+                reportApiError(data);
+                reject(data);
+            }
+        })
     })
 }
