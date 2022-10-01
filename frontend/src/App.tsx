8a5642c231c76c8500a81f7c3416386d8c6d6b7f
EshelSil <eshelsil@gmail.com>
2022-10-01 04:53:28 +0300

    [FE] - support RTL in MaterialUI components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from "history";
-import AppHeader from './appHeader/AppHeaderProvider';
 import { Provider as StoreProvider } from 'react-redux';
+import { ThemeProvider } from '@mui/material/styles';
+import AppHeader from './appHeader/AppHeaderProvider';
 import store from './_helpers/store';
 import AuthController from './auth/AuthController';
 import AppBody from './appContent/AppBody';
 import AppFooter from './appFooter/Footer';
 import Banner from './appBanner/AppBannerView';
 import DialogsProvider from './dialogs/DialogsProvider';
+import { theme } from './themes/theme';
 import './App.scss';
+import RTL from './_helpers/RTL';
 
 
 const customHistory = createBrowserHistory();
@@ -17,15 +20,19 @@ const customHistory = createBrowserHistory();
 function App() {
 	return (
 	<StoreProvider store={store}>
-		<AuthController>
-			<Router history={customHistory}>
-				<Banner />
-				<AppHeader />
-				<AppBody />
-				<AppFooter />
-				<DialogsProvider />
-			</Router>
-		</AuthController>
+		<ThemeProvider theme={theme}>
+			<RTL>
+				<AuthController>
+					<Router history={customHistory}>
+						<Banner />
+						<AppHeader />
+						<AppBody />
+						<AppFooter />
+						<DialogsProvider />
+					</Router>
+				</AuthController>
+			</RTL>
+		</ThemeProvider>
 	</StoreProvider>
 	);
 }
