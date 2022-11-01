93ee0fd985c75016dfc4533597a0666826f24509
EshelSil <eshelsil@gmail.com>
2022-11-01 13:16:45 +0200

    AppMenuMobile - close on click outside

--- frontend/src/appHeader/AppMenuMobile.tsx
+++ frontend/src/appHeader/AppMenuMobile.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { UtlWithTournament } from '../types'
 import Toolbar from '@mui/material/Toolbar'
 import Container from '@mui/material/Container'
-import { IconButton, Typography } from '@mui/material'
+import { ClickAwayListener, IconButton, Typography } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
 import CloseIcon from '@mui/icons-material/CloseRounded'
 import UserMenu from './UserMenu'
@@ -28,45 +28,47 @@ function AppMenuMobile({
 
 
     return (
-        <div  className="LigaBet-AppMenuMobile">
-            <Toolbar style={{minHeight: 'unset'}}>
-                <Container className="mobileMenuHeader">
-                    <IconButton
-                        size="large"
-                        aria-label="account of current user"
-                        aria-controls="menu-appbar"
-                        aria-haspopup="true"
-                        color="inherit"
-                        onClick={menuClickedHandler}
-                    >
-                        {menuOpen ? <CloseIcon /> : <MenuIcon />}
-                    </IconButton>
-                    <Typography variant="h5" className="appName">
-                        מונדיאל 2022
-                    </Typography>
-                    <UserMenu
-                        {
-                            ...{
-                                currentUtl,
-                                currentUsername,
-                                openDialogChangePassword,
+        <ClickAwayListener onClickAway={closeMenuHandler}>
+            <div className="LigaBet-AppMenuMobile">
+                <Toolbar style={{minHeight: 'unset'}}>
+                    <Container className="mobileMenuHeader">
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            color="inherit"
+                            onClick={menuClickedHandler}
+                        >
+                            {menuOpen ? <CloseIcon /> : <MenuIcon />}
+                        </IconButton>
+                        <Typography variant="h5" className="appName">
+                            מונדיאל 2022
+                        </Typography>
+                        <UserMenu
+                            {
+                                ...{
+                                    currentUtl,
+                                    currentUsername,
+                                    openDialogChangePassword,
+                                }
                             }
-                        }
-                    />
-                </Container>
-            </Toolbar>
-            {menuOpen && (
-                <Toolbar className="expandSection">
-                    <Container className="toolbarContainer-mobile">
-                        <TournamentMenuItems {...{
-                            isTournamentStarted,
-                            currentUtl,
-                            reRouteCallback: closeMenuHandler
-                        }}/>
+                        />
                     </Container>
                 </Toolbar>
-            )}
-        </div>
+                {menuOpen && (
+                    <Toolbar className="expandSection">
+                        <Container className="toolbarContainer-mobile">
+                            <TournamentMenuItems {...{
+                                isTournamentStarted,
+                                currentUtl,
+                                reRouteCallback: closeMenuHandler
+                            }}/>
+                        </Container>
+                    </Toolbar>
+                )}
+            </div>
+        </ClickAwayListener>
     )
 }
 
