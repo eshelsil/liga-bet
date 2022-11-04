1c738b127e7924a5c2b3fed4a57e728ba0d53d4f
EshelSil <eshelsil@gmail.com>
2022-11-04 11:08:03 +0200

    update utl name - implement loader

--- frontend/src/myUTLs/UtlCard.tsx
+++ frontend/src/myUTLs/UtlCard.tsx
@@ -8,11 +8,12 @@ import { UtlRole, UtlWithTournament } from '../types'
 import { isTournamentStarted, UtlRoleToString } from '../utils'
 import EditIcon from '@mui/icons-material/Edit'
 import CloseIcon from '@mui/icons-material/Close'
-import DoneIcon from '@mui/icons-material/Done'
 import useGoTo from '../hooks/useGoTo'
 import EmojiEventsOutlined from '@mui/icons-material/EmojiEventsOutlined'
 import { useTournamentLink } from '../hooks/useTournamentLink'
 import CopyLink from '../widgets/CopyToClipboard/CopyLink'
+import useCancelEdit from '../hooks/useCancelEdit'
+import LoadingVIcon from '../widgets/LoadingVIcon'
 
 interface Props {
     utl: UtlWithTournament
@@ -27,20 +28,26 @@ interface Props {
 }
 
 function UtlCard({ utl, utlIndex, isSelected,  updateUTL, selectUtl, socreboardRow }: Props) {
-    const { goToMyBets, goToTakanon, goToInviteFriends } = useGoTo()
+    const { goToMyBets, goToTakanon } = useGoTo()
     const joinLink = useTournamentLink(utl.tournament.code);
     const [edit, setEdit] = useState(false)
     const [name, setName] = useState(utl.name)
+    const { getLastEditTs, cancelEdit } = useCancelEdit({edit, setEdit})
 
     const isTournamentAdmin = utl.role === UtlRole.Admin
     const canInviteFriends = isTournamentStarted && !isTournamentStarted(utl.tournament)
     const toggleEdit = () => setEdit(!edit)
 
-    const update = () => {
-        updateUTL({ name }).then(() => {
-            setEdit(false)
-            window['toastr']['success']('הפרופיל עודכן')
-        })
+    const update = async () => {
+        const ts = getLastEditTs()
+        await updateUTL({ name })
+            .then(() => {
+                window['toastr']['success']('הפרופיל עודכן')
+                cancelEdit(ts)
+            })
+            .catch(function (error) {
+                console.log('FAILED updating UTL', error)
+            })
     }
 
     const updateName = (e) => setName(e.target.value)
@@ -136,9 +143,7 @@ function UtlCard({ utl, utlIndex, isSelected,  updateUTL, selectUtl, socreboardR
                 </div>
                 <div className='buttonsContinaer'>
                     {edit && (
-                        <IconButton className="confirmButton" onClick={update}>
-                            <DoneIcon />
-                        </IconButton>
+                        <LoadingVIcon action={update} />
                     )}
                     <IconButton className="editButton" onClick={toggleEdit}>
                         {!edit && <EditIcon />}
