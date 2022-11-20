dfdedd47251a49c1c2fe305aa8252f752bd8225a
EshelSil <eshelsil@gmail.com>
2022-11-20 03:04:45 +0200

    update-live-scorers - fix tests

--- app/Actions/UpdateLeaderboards.php
+++ app/Actions/UpdateLeaderboards.php
@@ -32,8 +32,11 @@ class UpdateLeaderboards
         if (config("test.onlyTournamentId") && config("test.onlyTournamentId") != $tournament->id) {
             return;
         }
-
         \Log::debug("[UpdateLeaderboards][handle] Started!!!");
+
+        $version = $tournament->leaderboardVersions->firstWhere('game_id', $gameId);
+        if ($version) return;
+
         $betsScoreSum = $tournament->bets()
                      ->select(["user_tournament_id", DB::raw("COALESCE(sum(bets.score), 0) as total_score")])
                      ->groupBy(["user_tournament_id"])
@@ -56,17 +59,15 @@ class UpdateLeaderboards
                 $rank = $i + 1;
             }
 
-            $leader = $version->leaderboards->first(fn($l) => $l->user_tournament_id == $userScore->user_tournament_id);
-            if (!$leader) {
-                $leader = new Leaderboard();
-                $leader->tournament_id      = $tournament->id;
-                $leader->user_tournament_id = $userScore->user_tournament_id;
-                $leader->version_id         = $version->id;
-            }
+            $leader = new Leaderboard();
+            $leader->tournament_id      = $tournament->id;
+            $leader->user_tournament_id = $userScore->user_tournament_id;
+            $leader->version_id         = $version->id;
             $leader->rank               = $rank;
             $leader->score = $lastScore = $userScore->total_score;
             $leader->save();
         }
+        $version->refresh();
 
         // TODO: Users with no bets at all?
     }
@@ -79,15 +80,11 @@ class UpdateLeaderboards
     }
 
     private function getGamesToConsider($version, $updatedGameIds, $gameIdsByLeaderboardVersionOrder){
-        $versionIndex = $gameIdsByLeaderboardVersionOrder->search(fn($id) => $id == $version->id);
-        $gamesToConsider = [];
-        $updatedGameIds->each(function($id) use ($gameIdsByLeaderboardVersionOrder, $versionIndex){
+        $versionIndex = $gameIdsByLeaderboardVersionOrder->search(fn($id) => $id == $version->game_id);
+        return $updatedGameIds->filter(function($id) use ($gameIdsByLeaderboardVersionOrder, $versionIndex){
             $index = $gameIdsByLeaderboardVersionOrder->search(fn($gameId) => $gameId == $id);
-            if ($index <= $versionIndex) {
-                $gamesToConsider[] = $id;
-            }
+            return $index <= $versionIndex;
         });
-        return $gamesToConsider;
     }
 
     private function getScoresDiffByUtlId(
@@ -99,33 +96,34 @@ class UpdateLeaderboards
         $updatedGameIds,
         $gameIdsByLeaderboardVersionOrder
     ){
+        Log::debug(['$utlsByTopScorerId' => $utlsByTopScorerId->toJson(), '$utlsByTopAssitsId' =>$utlsByTopAssitsId->toJson()]);
 
         $eachGoal = array_get($tournament->config, "scores.specialBets.topScorer.eachGoal", 0);
         $eachAssist = array_get($tournament->config, "scores.specialBets.topAssists.eachGoal", 0);
-        $scorersScoreDiffByPlayerId = [];
-        $assistsScoreDiffByPlayerId = [];
+        $scorersScoreDiffByPlayerId = collect([]);
+        $assistsScoreDiffByPlayerId = collect([]);
         $gamesToConsider = $this->getGamesToConsider($version, $updatedGameIds, $gameIdsByLeaderboardVersionOrder);
         foreach($gamesToConsider as $gameId){
             $newScorersData = collect(data_get($scorersByGameId[$gameId], 'scorers', []))
                 ->map(fn($goals)=> $goals * $eachGoal);
             $newAssistsData = collect(data_get($scorersByGameId[$gameId], 'assists', []))
-                ->map(fn($goals)=> $goals * $eachAssist);
-            array_merge($scorersScoreDiffByPlayerId, $newScorersData);
-            array_merge($assistsScoreDiffByPlayerId, $newAssistsData);
+            ->map(fn($goals)=> $goals * $eachAssist);
+            $scorersScoreDiffByPlayerId = $newScorersData->union($scorersScoreDiffByPlayerId);
+            $assistsScoreDiffByPlayerId = $newAssistsData->union($assistsScoreDiffByPlayerId);
         }
 
-        $scoreDiffByUtlId = [];
-        foreach ($scorersScoreDiffByPlayerId as $playerId => $scoreDiff) {
-            foreach($utlsByTopScorerId[$playerId] as $utlId){
-                if (!array_key_exists($utlId, $scoreDiffByUtlId)){
+        $scoreDiffByUtlId = collect([]);
+        foreach (($scorersScoreDiffByPlayerId ?? []) as $playerId => $scoreDiff) {
+            foreach(collect($utlsByTopScorerId)->get($playerId, []) as $utlId){
+                if (!$scoreDiffByUtlId->has($utlId)){
                     $scoreDiffByUtlId[$utlId] = 0;
                 }
                 $scoreDiffByUtlId[$utlId] += $scoreDiff;
             }
         }
-        foreach ($assistsScoreDiffByPlayerId as $playerId => $scoreDiff) {
-            foreach($utlsByTopScorerId[$playerId] as $utlId){
-                if (!array_key_exists($utlId, $scoreDiffByUtlId)){
+        foreach (($assistsScoreDiffByPlayerId ?? []) as $playerId => $scoreDiff) {
+            foreach(collect($utlsByTopAssitsId)->get($playerId, []) as $utlId){
+                if (!$scoreDiffByUtlId->has($utlId)){
                     $scoreDiffByUtlId[$utlId] = 0;
                 }
                 $scoreDiffByUtlId[$utlId] += $scoreDiff;
@@ -142,7 +140,8 @@ class UpdateLeaderboards
         }
         
         $updatedGameIds = $scorersByGameId->keys();
-        $allVersions = $tournament->leaderboardVersions->sortBy('created_at');
+        $tournament->refresh();
+        $allVersions = $tournament->leaderboardVersions->sortBy('id');
         $earliestVersion = $allVersions->first(fn($version) => $updatedGameIds->contains($version->game_id));
         if (!$earliestVersion){
             return;
@@ -155,8 +154,10 @@ class UpdateLeaderboards
         $topScorerAndAssistsBets = $tournament
             ->load([
                 "bets" => function (HasMany $query) use ($scorerSpecialBet, $assistsSpecialBet) {
-                    $query->where(['type' => BetTypes::SpecialBet, "type_id" => $scorerSpecialBet->id]);
-                    $query->orWhere(['type' => BetTypes::SpecialBet, "type_id" => $assistsSpecialBet->id]);
+                    $query->where(function($q) use ($scorerSpecialBet, $assistsSpecialBet) {
+                        $q->where(['type' => BetTypes::SpecialBet, "type_id" => $scorerSpecialBet->id])
+                            ->orWhere(['type' => BetTypes::SpecialBet, "type_id" => $assistsSpecialBet->id]);
+                    });
                 }
             ])->bets;
         $utlsByTopScorerId = $topScorerAndAssistsBets
@@ -169,7 +170,6 @@ class UpdateLeaderboards
             ->groupBy(fn($bet) => $bet->getAnswer())
             ->map(fn($bets) => $bets->map(fn($bet) => $bet->user_tournament_id));
         
-    
         $versionsToUpdate->each(function($version) use (
             $tournament,
             $scorersByGameId,
@@ -196,19 +196,20 @@ class UpdateLeaderboards
     {
         $hasChanges = false;
         $scoreboardRows = $version->leaderboards;
+        $scoreboardWithNewScores = collect([]);
         foreach($scoreDiffByUtlId as $utlId => $scoreDiff){
             if ($scoreDiff != 0){
                 $hasChanges = true;
             }
-            $leader = $scoreboardRows->firstWhere(fn($l) => $l->user_tournament_id == $utlId);
+            $leader = $scoreboardRows->first(fn($l) => $l->user_tournament_id == $utlId);
             $leader->score += $scoreDiff;
-            Log::debug("Added $scoreDiff scores to scoreboard-row of utl $utlId on version $version->id");
+            $scoreboardWithNewScores[] = $leader;
         }
         if ($hasChanges){
             $lastScore = null;
             $rank = null;
             $scoreboardRows->sortByDesc('score')->each(function($leader, $i) use($rank, $lastScore, $version) {
-                if ($lastScore != $leader->score) {
+                if (is_null($lastScore) || $lastScore != $leader->score) {
                     $rank = $i + 1;
                 }
                 $leader->rank = $rank;
