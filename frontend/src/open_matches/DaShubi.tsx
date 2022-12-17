2c59a07a94d6bafc04ea43b8dfa18d28f0125496
EshelSil <eshelsil@gmail.com>
2022-12-17 15:28:14 +0200

    fix DaShubi on mobile

--- frontend/src/open_matches/DaShubi.tsx
+++ frontend/src/open_matches/DaShubi.tsx
@@ -18,22 +18,25 @@ const next = () => {
 
 const DaShubi = ({dismiss}: Props) => {
     const [shown, setShown] = useState(false)
+    const [imgLoaded, setImgLoaded] = useState(false)
     const imgRef = useRef(imgs[imgIndex])
     const img = imgRef.current
     useEffect(() => {
-        next()
-        setTimeout(
-            () => setShown(true)
-            , 1
-        )
-        const autoDismissTimeout = setTimeout(dismiss, 4000)
-        return () => {
-            clearTimeout(autoDismissTimeout)
+        if (imgLoaded) {
+            next()
+            setTimeout(
+                () => setShown(true)
+                , 1
+            )
+            const autoDismissTimeout = setTimeout(dismiss, 4000)
+            return () => {
+                clearTimeout(autoDismissTimeout)
+            }
         }
-    }, [])
+    }, [imgLoaded])
     return (
         <div className={`LB-DaShubi ${shown ? 'DaShubi-shown' : ''}`}>
-            <img onClick={dismiss} className='DaShubi-img' src={`img/${img}`}/>
+            <img onClick={dismiss} onLoad={() => setImgLoaded(true)} className='DaShubi-img' src={`img/${img}`}/>
         </div>
     )
 }
