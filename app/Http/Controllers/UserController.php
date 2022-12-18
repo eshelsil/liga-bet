df0e5ad00705e36c219681e9a531d10f6d965329
EshelSil <eshelsil@gmail.com>
2022-12-18 16:11:06 +0200

    UserController - disable API for update-competition by user ping

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -147,23 +147,23 @@ class UserController extends Controller
 
     public function ping(UpdateCompetition $uc)
     {
-        try {
-            Cache::remember("updateCompetition", now()->addMinutes(2), function () use ($uc) {
-                return Cache::lock("updateCompetition:lock", 60)
-                    ->block(0, function () use ($uc){
-                        $this->getUser()
-                            ->utls
-                            ->load("tournament.competition")
-                            ->map(fn(TournamentUser $utl) => $utl->tournament->competition)
-                            ->unique()
-                            ->each(fn(Competition $competition) => $uc->handle($competition));
+        // try {
+        //     Cache::remember("updateCompetition", now()->addMinutes(2), function () use ($uc) {
+        //         return Cache::lock("updateCompetition:lock", 60)
+        //             ->block(0, function () use ($uc){
+        //                 $this->getUser()
+        //                     ->utls
+        //                     ->load("tournament.competition")
+        //                     ->map(fn(TournamentUser $utl) => $utl->tournament->competition)
+        //                     ->unique()
+        //                     ->each(fn(Competition $competition) => $uc->handle($competition));
     
-                        return true;
-                    });
-            });
-        } catch (\Throwable $e) {
-            return response("SERVER_ERROR_MSG:".$e->getMessage()."TRACE: ".$e->getTraceAsString(), 500);
-        }
+        //                 return true;
+        //             });
+        //     });
+        // } catch (\Throwable $e) {
+        //     return response("SERVER_ERROR_MSG:".$e->getMessage()."TRACE: ".$e->getTraceAsString(), 500);
+        // }
 
         return new JsonResponse(["pong"]);
     }
