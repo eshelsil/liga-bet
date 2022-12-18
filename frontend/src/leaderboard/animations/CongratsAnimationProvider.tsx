4cb1f70a96496ad7af6b18fb989341e15b415010
EshelSil <eshelsil@gmail.com>
2022-12-18 20:57:11 +0200

    FE - render congratsAniumation only after leaderboards-versions are loaded

--- frontend/src/leaderboard/animations/CongratsAnimationProvider.tsx
+++ frontend/src/leaderboard/animations/CongratsAnimationProvider.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { UtlBase } from '../../types'
-import { CurrentTournamentId, IsCompetitionDone, IsMissingMvpAnswer, IsOurTournament } from '../../_selectors'
+import { CurrentTournamentId, IsCompetitionDone, IsMissingMvpAnswer, IsOurTournament, LeaderboardVersions } from '../../_selectors'
 import CongratsAnimation from './CongratsAnimation'
 import dayjs from 'dayjs'
 import './Animation.scss'
@@ -17,10 +17,11 @@ function CongratsAnimationProvider({ currentUtl, rank }: Props) {
     const tournamentId = useSelector(CurrentTournamentId)
     const isOurTournament = useSelector(IsOurTournament)
     const isCompetitionDone = useSelector(IsCompetitionDone)
+    const leaderboardVersions = useSelector(LeaderboardVersions)
     const isMvpMissing = useSelector(IsMissingMvpAnswer)
     const tournamentDone = isCompetitionDone && !isMvpMissing
 
-    const seenCongratsData = localStorage.getItem('LigaBetSeenCongratsAnimation')
+    const seenCongratsData = localStorage.getItem('LigaBetSeenCongratsAnimationV2')
     let lastSeenPerTournamentId: Record<number, number> = seenCongratsData ? JSON.parse(seenCongratsData) : {}
     const lastSeenTimestamp = lastSeenPerTournamentId[tournamentId]
     const seenRecently = lastSeenTimestamp ? (dayjs().diff(dayjs(lastSeenTimestamp), 'hours') < 72) : false
@@ -28,13 +29,14 @@ function CongratsAnimationProvider({ currentUtl, rank }: Props) {
     const onSeenAnimation = () => {
         if (seenCongratsData) {
             lastSeenPerTournamentId[tournamentId] = Number(new Date())
-            localStorage.setItem('LigaBetSeenCongratsAnimation', JSON.stringify(lastSeenPerTournamentId))
+            localStorage.setItem('LigaBetSeenCongratsAnimationV2', JSON.stringify(lastSeenPerTournamentId))
         } else {
-            localStorage.setItem('LigaBetSeenCongratsAnimation', JSON.stringify({[tournamentId]: Number(new Date())}))
+            localStorage.setItem('LigaBetSeenCongratsAnimationV2', JSON.stringify({[tournamentId]: Number(new Date())}))
         }
     }
-
-    const showCongratsAnimation = tournamentDone && (isOurTournament || rank === 1)
+    
+    const loadedLeaderboards = Object.keys(leaderboardVersions).length > 0
+    const showCongratsAnimation = tournamentDone && (isOurTournament || rank === 1) && loadedLeaderboards
     return (<>
         {showCongratsAnimation && !seenRecently && (
             <CongratsAnimation currentUtl={currentUtl} rank={rank} onSeenAnimation={onSeenAnimation} />
