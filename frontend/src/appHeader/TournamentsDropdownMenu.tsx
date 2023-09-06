b7d550513671dcd99a517ec0fc1df24dc873a373
Eshel <eshelsil@gmail.com>
2023-09-07 02:02:46 +0300

    FE: TournamentDropDownMenu - support multi-competitions

--- frontend/src/appHeader/TournamentsDropdownMenu.tsx
+++ frontend/src/appHeader/TournamentsDropdownMenu.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DropMenuItem from './DropMenuItem'
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import HistoryIcon from '@mui/icons-material/History';
+import RedoIcon from '@mui/icons-material/Redo';
 import { connect, useSelector } from 'react-redux'
-import { CurrentTournament, NoSelector, TournamentsWithMyUtl, CanCreateNewTournament, CanJoinAnotherTournament, Notifications, HasNotificationsOnOtherTournaments } from '../_selectors'
+import { CurrentTournament, NoSelector, CanCreateNewTournament, CanJoinAnotherTournament, Notifications, HasNotificationsOnOtherTournaments, OldUTLsByCompetitionId, LiveUTLsByCompetitionId, HasOpenCompetitions } from '../_selectors'
 import { selectUtl } from '../_actions/tournamentUser'
 import MenuItem from '@mui/material/MenuItem'
 import Button from '@mui/material/Button'
@@ -11,7 +13,7 @@ import useGoTo from '../hooks/useGoTo';
 import EmojiEventsOutlined from '@mui/icons-material/EmojiEventsOutlined'
 import { orderBy } from 'lodash';
 import { Badge } from '@mui/material';
-import { isTournamentStarted } from '../utils';
+import { keysOf, valuesOf } from '../utils';
 
 
 function IconWithNotification({hasNotifications}: {hasNotifications: boolean}){
@@ -37,19 +39,28 @@ function TournamentsDropdownMenu({
     itemClickCallback,
 }: Props) {
     const { goToJoinTournament, goToCreateTournament } = useGoTo()
-    const tournamentsById = useSelector(TournamentsWithMyUtl);
+
+    const oldUtlsByCompId = useSelector(OldUTLsByCompetitionId);
+    const liveUtlsByCompId = useSelector(LiveUTLsByCompetitionId);
     const selectedTournament = useSelector(CurrentTournament);
     const notificaitons = useSelector(Notifications);
+    const hasOpenCompetitions = useSelector(HasOpenCompetitions);
     const hasNotificationsOnOtherTournaments = useSelector(HasNotificationsOnOtherTournaments)
     const canJoinAnotherTournament = useSelector(CanJoinAnotherTournament);
     const canCreateNewTournament = useSelector(CanCreateNewTournament);
 
-    const tournaments = Object.values(tournamentsById);
-    const sortedTournaments = orderBy(tournaments, t => t.linkedUtl.createdAt)
-    const hasOnlyOneTournamnet = tournaments.length === 1
-    const showNotifciations = !hasOnlyOneTournamnet && hasNotificationsOnOtherTournaments
+    const [isLive, setIsLive] = useState(true)
+    const utlsByCompId = isLive ? liveUtlsByCompId : oldUtlsByCompId
+    const hasOldBets = keysOf(oldUtlsByCompId).length > 0
+    const showHistoryButton = isLive && hasOldBets
+    const showLiveButton = !isLive
 
-    const tournamentStarted = isTournamentStarted(selectedTournament)
+    const competitions = valuesOf(utlsByCompId).map(utls => utls[0].tournament.competition);
+    const sortedCompetitions = orderBy(competitions, comp => comp.startTime, 'desc');
+    
+
+    const showNotifciations = isLive && hasNotificationsOnOtherTournaments
+
     
 
     const onTournamentItemClick = (utlId: number) => {
@@ -64,7 +75,10 @@ function TournamentsDropdownMenu({
         itemClickCallback && itemClickCallback()
         goToCreateTournament()
     }
-    
+    const toggleHistoryMode = () => {
+        setIsLive(!isLive)
+    }
+
     return (
         <DropMenuItem
             anchorContent={
@@ -75,35 +89,64 @@ function TournamentsDropdownMenu({
             }
             classes={{list: 'tournamentsMenu'}}
         >
-            {sortedTournaments.map(tournament => (
-                <TournamentItemLink
-                    key={tournament.id}
-                    tournament={tournament}
-                    selected={tournament.id === selectedTournament.id}
-                    onClick={() => onTournamentItemClick(tournament.linkedUtl.id)}
-                    notifications={showNotifciations ? notificaitons[tournament.id] : 0}
-                />
+            {sortedCompetitions.map(competition => (
+                <div key={competition.id} className="TournamentMenu-Competition">
+                    <div onClick={(e) => e.stopPropagation()} className="TournamentMenu-CompetitionTitle">{competition.name}</div>
+                    {orderBy(utlsByCompId[competition.id], 'createdAt').map(utl => (
+                        <TournamentItemLink
+                            key={utl.tournament.id}
+                            tournament={{...utl.tournament, linkedUtl: utl}}
+                            selected={utl.tournament.id === selectedTournament.id}
+                            onClick={() => onTournamentItemClick(utl.id)}
+                            notifications={showNotifciations ? notificaitons[utl.tournament.id] : 0}
+                        />
+                    ))}
+                </div>
             ))}
-            {canJoinAnotherTournament && !tournamentStarted && (
-                <MenuItem
+            {showHistoryButton && (
+                <div
                     className={`buttonLink`}
-                    onClick={joinTournament}
+                    onClick={(e) => e.stopPropagation()}
                 >
-                    <Button variant='contained' color='primary'>
-                        הצטרף לטורניר נוסף
-                    </Button>
-                </MenuItem>
+                    <div className='changeLiveMode' onClick={toggleHistoryMode}>
+                        <HistoryIcon />
+                        Show history
+                    </div>
+                </div>
             )}
-            {canCreateNewTournament && !tournamentStarted && (
-                <MenuItem
+            {showLiveButton && (
+                <div
                     className={`buttonLink`}
-                    onClick={createTournament}
+                    onClick={(e) => e.stopPropagation()}
                 >
-                    <Button variant='contained' color='primary'>
-                            צור טורניר חדש
-                    </Button>
-                </MenuItem>
+                    <div className='changeLiveMode' onClick={toggleHistoryMode}>
+                        <RedoIcon />
+                        Show Live
+                    </div>
+                </div>
             )}
+            {isLive && (<div>
+                {canJoinAnotherTournament && hasOpenCompetitions && (
+                    <MenuItem
+                        className={`buttonLink`}
+                        onClick={joinTournament}
+                    >
+                        <Button variant='contained' color='primary'>
+                            הצטרף לטורניר נוסף
+                        </Button>
+                    </MenuItem>
+                )}
+                {canCreateNewTournament && hasOpenCompetitions && (
+                    <MenuItem
+                        className={`buttonLink`}
+                        onClick={createTournament}
+                    >
+                        <Button variant='contained' color='primary'>
+                                צור טורניר חדש
+                        </Button>
+                    </MenuItem>
+                )}
+            </div>)}
         </DropMenuItem>
     )
 }
