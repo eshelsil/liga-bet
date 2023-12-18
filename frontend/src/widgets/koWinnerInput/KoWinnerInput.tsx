aaf71b4acb0bd278ac9b4350e6dd94bef7416f13
Eshel <eshelsil@gmail.com>
2023-12-18 03:27:06 +0200

    [FE] koWinnerInput - update styling for twoLegsTie

--- frontend/src/widgets/koWinnerInput/KoWinnerInput.tsx
+++ frontend/src/widgets/koWinnerInput/KoWinnerInput.tsx
@@ -6,10 +6,13 @@ import './KoWinnerInput.scss'
 interface Props {
     value: WinnerSide
     setValue: (side: WinnerSide | null) => void
+    isTwoLegKo?: boolean
+    isMissing?: boolean
+    disabled?: boolean
     onlyDisplay?: boolean
 }
 
-function KoWinnerInput({ value, setValue, onlyDisplay = false }: Props) {
+function KoWinnerInput({ value, setValue, isTwoLegKo = false, isMissing = false, disabled = false, onlyDisplay = false }: Props) {
     const onChange = (winner: WinnerSide) => {
         setValue(winner)
     }
@@ -20,22 +23,26 @@ function KoWinnerInput({ value, setValue, onlyDisplay = false }: Props) {
     const isAwayWinner = value === WinnerSide.Away
 
     return (
-        <div className={`LB-KoWinnerInput ${onlyDisplay ? 'KoWinnerInput-displayOnly' : ''}`}>
+        <div className={`LB-KoWinnerInput ${onlyDisplay ? 'KoWinnerInput-displayOnly' : ''} ${isTwoLegKo ? 'KoWinnerInput-twoLegKo' : ''} ${disabled ? 'KoWinnerInput-disabled' : ''}`}>
             <div className="KoWinnerInput-content">
                 <div
                     className={`KoWinnerInput-side KoWinnerInput-home ${isHomeWinner ? 'KoWinnerInput-selected' : ''}`}
-                    onClick={onlyDisplay ? null : setHomeWinner}
+                    onClick={(disabled || onlyDisplay) ? null : setHomeWinner}
                 >
                     <div className='KoWinnerInput-button'>
                         ✌️
                     </div>
                 </div>
                 <div className={`KoWinnerInput-delimiter`}>
-                    מעפילה
+                    מעפילה {
+                        isMissing ? (
+                            <span style={{position: 'absolute', fontSize: 12, opacity:0.5, marginRight: 12, lineHeight: '24px'}}>(חסר)</span>
+                        ): null
+                    }
                 </div>
                 <div
                     className={`KoWinnerInput-side KoWinnerInput-away ${isAwayWinner ? 'KoWinnerInput-selected' : ''}`}
-                    onClick={onlyDisplay ? null : setAwayWinner}
+                    onClick={(disabled || onlyDisplay) ? null : setAwayWinner}
                 >
                     <div className='KoWinnerInput-button'>
                         ✌️
