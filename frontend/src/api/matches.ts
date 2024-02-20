eb264948a9a0aabd9f4be46d4597364e5631c368
Eshel <eshelsil@gmail.com>
2024-02-20 04:08:36 +0200

    fixup for deprecate fetchAPIGames

--- frontend/src/api/matches.ts
+++ frontend/src/api/matches.ts
@@ -13,29 +13,6 @@ export const fetchMatches = async (
     })
 }
 
-export const updateScoresFromApi = async () => {
-    ;(window as any).$.ajax({
-        type: 'GET',
-        url: '/api-fetch-games',
-        success: function (server_msg) {
-            const server_success_divider = 'SERVER_SUCCESS_MSG:'
-            const msg = server_msg.split(server_success_divider)[1]
-            ;(window as any).toastr['success'](msg)
-        },
-        error: function (error) {
-            console.log('error', error)
-            const error_text = error.responseText
-            const server_error_divider = 'SERVER_ERROR_MSG:'
-            if (error_text.indexOf(server_error_divider) > -1) {
-                const error_msg = error_text.split(server_error_divider)[1]
-                ;(window as any).toastr['error'](error_msg)
-            } else {
-                ;(window as any).toastr['error'](error_text)
-            }
-        },
-    })
-}
-
 export const fetchGamesGoalsData = async (
     tournamentId: number
 ): Promise<GameGoalsData[]> => {
