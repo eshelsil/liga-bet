039ae4ba6762d18b480d773baee109ce6d864316
Eshel <eshel@carefam.com>
2026-06-27 19:20:55 +0300

    leaderboard - show runner-up as well

--- frontend/src/_selectors/logic/winnerBet.ts
+++ frontend/src/_selectors/logic/winnerBet.ts
@@ -1,16 +1,24 @@
 import { mapValues } from 'lodash'
 import { createSelector } from 'reselect'
-import { WinnerSpecialQuestionId } from '../base'
-import {
-    QuestionBetsByUtlId,
-} from '../modelRelations'
+import { RunnerUpSpecialQuestionId, WinnerSpecialQuestionId } from '../base'
+import { QuestionBetsByUtlId } from '../modelRelations'
 
 export const WinnerBetByUtlId = createSelector(
     QuestionBetsByUtlId,
     WinnerSpecialQuestionId,
     (questionBetsByUtlId, winnerQuestionId) => {
-        return mapValues(questionBetsByUtlId,
-            bets => bets.find(bet => bet.type_id === winnerQuestionId)
+        return mapValues(questionBetsByUtlId, (bets) =>
+            bets.find((bet) => bet.type_id === winnerQuestionId)
+        )
+    }
+)
+
+export const RunnerUpBetByUtlId = createSelector(
+    QuestionBetsByUtlId,
+    RunnerUpSpecialQuestionId,
+    (questionBetsByUtlId, runnerUpQuestionId) => {
+        return mapValues(questionBetsByUtlId, (bets) =>
+            bets.find((bet) => bet.type_id === runnerUpQuestionId)
         )
     }
 )
--- frontend/src/leaderboard/LeaderboardTable.tsx
+++ frontend/src/leaderboard/LeaderboardTable.tsx
@@ -1,38 +1,55 @@
 import React, { useContext } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux'
 import { QuestionBetWithRelations, ScoreboardRowDetailed, Team } from '../types'
 import CustomTable from '../widgets/Table/CustomTable'
-import ArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import AddIcon from '@mui/icons-material/Add';
+import ArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import AddIcon from '@mui/icons-material/Add'
 import ExpandedContestant from './ExpandedContestantProvider'
 import { usePrizesThemeClass } from '../hooks/useThemeClass'
-import { getRankDisplayById } from './utils';
-import TeamFlag from '../widgets/TeamFlag/TeamFlag';
-import { WinnerBetByUtlId } from '../_selectors';
-import { ExpandedContestantContext } from './ExpandedContestantContext';
-
+import { getRankDisplayById } from './utils'
+import TeamFlag from '../widgets/TeamFlag/TeamFlag'
+import { RunnerUpBetByUtlId, WinnerBetByUtlId } from '../_selectors'
+import { ExpandedContestantContext } from './ExpandedContestantContext'
+import { cn } from '@/utils/tailwind'
 
 export function NameWithWinnerFlag({
     name,
     winnerBet,
-    flagSize = 32,
-} : {
-    name: string,
-    winnerBet: QuestionBetWithRelations,
-    flagSize?: number,
-}){
+    runnerUpBet,
+}: {
+    name: string
+    winnerBet: QuestionBetWithRelations
+    runnerUpBet?: QuestionBetWithRelations
+}) {
     return (
-        <div className='LB-NameWithWinnerFlag'>
-            {winnerBet && (
-                <TeamFlag team={winnerBet.answer as Team} size={flagSize} />
+        <div className="LB-NameWithWinnerFlag">
+            {(winnerBet?.answer as Team)?.is_club && (
+                <TeamFlag team={winnerBet.answer as Team} size={36} />
             )}
-            {!winnerBet && (
-                <div className='NameWithWinnerFlag-noWinner' />
+            {!(winnerBet?.answer as Team)?.is_club && (
+                <div
+                    className={cn('LB-TeamFlag-wrapper', {
+                        '!shadow-none': !winnerBet && !runnerUpBet,
+                    })}
+                >
+                    {winnerBet && (
+                        <TeamFlag team={winnerBet.answer as Team} size={36} />
+                    )}
+                    {!winnerBet && (
+                        <div className="NameWithWinnerFlag-noWinner" />
+                    )}
+                    {runnerUpBet && (
+                        <div className="NameWithWinnerFlag-runnerUp">
+                            <TeamFlag
+                                team={runnerUpBet.answer as Team}
+                                size={20}
+                            />
+                        </div>
+                    )}
+                </div>
             )}
-            <div className="NameWithWinnerFlag-name">
-                {name}
-            </div>
+            <div className="NameWithWinnerFlag-name">{name}</div>
         </div>
     )
 }
@@ -46,16 +63,19 @@ interface Props {
 function LeaderboardTable({ rows, currentUtlId, isLive }: Props) {
     const { t } = useTranslation('leaderboard')
     const winnerBetByUtlId = useSelector(WinnerBetByUtlId)
-    const { expandedUtl: expand, setExpandedUtl: setExpand } = useContext(ExpandedContestantContext);
+    const runnerUpBetByUtlId = useSelector(RunnerUpBetByUtlId)
+    const { expandedUtl: expand, setExpandedUtl: setExpand } = useContext(
+        ExpandedContestantContext
+    )
 
     const getPrizeTheme = usePrizesThemeClass()
-    const hasScores = !!rows.find(row => row.score > 0)
+    const hasScores = !!rows.find((row) => row.score > 0)
     const rankDisplayById = getRankDisplayById(rows)
 
     const cells = [
-		{
-			id: 'rankChange',
-			header: '',
+        {
+            id: 'rankChange',
+            header: '',
             classes: {
                 header: 'rankChangeCell',
                 cell: 'rankChangeCell',
@@ -63,79 +83,85 @@ function LeaderboardTable({ rows, currentUtlId, isLive }: Props) {
             getter: (model: ScoreboardRowDetailed) => (
                 <>
                     {!!model.change && (
-                        <div className={`rankChange ${model.change < 0 ? 'isNegative' : ''}`}>
-                            <span className='rankChange-value'>{Math.abs(model.change)}</span>
-                            <ArrowDownIcon className='rankChange-direction'/>
+                        <div
+                            className={`rankChange ${
+                                model.change < 0 ? 'isNegative' : ''
+                            }`}
+                        >
+                            <span className="rankChange-value">
+                                {Math.abs(model.change)}
+                            </span>
+                            <ArrowDownIcon className="rankChange-direction" />
                         </div>
                     )}
                 </>
             ),
-		},
-		{
-			id: 'rank',
-			header: '',
+        },
+        {
+            id: 'rank',
+            header: '',
             classes: {
                 header: 'rankCell',
                 cell: 'rankCell',
             },
             getter: (model: ScoreboardRowDetailed) => rankDisplayById[model.id],
-		},
-		{
-			id: 'name',
-			header: t('table.headers.name'),
+        },
+        {
+            id: 'name',
+            header: t('table.headers.name'),
             classes: {
                 cell: 'nameCell',
             },
-			getter: (model: ScoreboardRowDetailed) => (
+            getter: (model: ScoreboardRowDetailed) => (
                 <NameWithWinnerFlag
                     name={model.name}
                     winnerBet={winnerBetByUtlId[model.user_tournament_id]}
+                    runnerUpBet={runnerUpBetByUtlId[model.user_tournament_id]}
                 />
             ),
-		},
-		{
-			id: 'score',
-			header: t('table.headers.score'),
+        },
+        {
+            id: 'score',
+            header: t('table.headers.score'),
             classes: {
                 header: 'scoreCell',
             },
-			getter: (model: ScoreboardRowDetailed) => (
-                <div className='scoreCell-container'>
-                    <div className='scoreCell-total'>
-                        {model.score}
-                    </div>
+            getter: (model: ScoreboardRowDetailed) => (
+                <div className="scoreCell-container">
+                    <div className="scoreCell-total">{model.score}</div>
                     {!!model.addedScore && (
-                        <div className='scoreCell-added'>
+                        <div className="scoreCell-added">
                             <span>{model.addedScore}</span>
-                            <AddIcon className='scoreCell-addIcon'/>
+                            <AddIcon className="scoreCell-addIcon" />
                         </div>
                     )}
                 </div>
             ),
-		},
+        },
     ]
 
     const getRowClassName = (model: ScoreboardRowDetailed) => {
-        const currentUtl = model.user_tournament_id === currentUtlId ? 'currentUtl' : ''
+        const currentUtl =
+            model.user_tournament_id === currentUtlId ? 'currentUtl' : ''
         const prizeClass = hasScores ? getPrizeTheme(model.rank) : ''
         return `${currentUtl} ${prizeClass}`
     }
-    
+
     const onRowClick = (model: ScoreboardRowDetailed) => {
-        if (expand === model.user_tournament_id){
+        if (expand === model.user_tournament_id) {
             setExpand(null)
         } else {
             setExpand(model.user_tournament_id)
         }
     }
 
-    const getExpandContent = (model: ScoreboardRowDetailed) => (
-        model.user_tournament_id === expand
-        ? (
-            <ExpandedContestant utlId={model.user_tournament_id} isLive={isLive} />
+    const getExpandContent = (model: ScoreboardRowDetailed) =>
+        model.user_tournament_id === expand ? (
+            <ExpandedContestant
+                utlId={model.user_tournament_id}
+                isLive={isLive}
+            />
         ) : null
-    )
-    
 
     return (
         <CustomTable
