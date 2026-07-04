b83e3ef97ca81b2eb911516b3978298c873346d8
Eshel <eshel@carefam.com>
2026-07-04 06:35:38 +0300

    fix special-role-badge away

--- frontend/src/widgets/MatchResult/MatchResultView.tsx
+++ frontend/src/widgets/MatchResult/MatchResultView.tsx
@@ -10,87 +10,87 @@ import './MatchResult.scss'
 import AutoBetBadge from '../AutoBetBadge/AutoBetBadge'
 import SpecialRoleBadge from '../SpecialRoleBadge/SpecialRoleBadge'
 
-
-function MatchResultView({home, away, isKnockout, qualifier, isAutoBet, title, homeRole, awayRole}: MatchResultProps){
-
+function MatchResultView({
+    home,
+    away,
+    isKnockout,
+    qualifier,
+    isAutoBet,
+    title,
+    homeRole,
+    awayRole,
+}: MatchResultProps) {
     const { t } = useTranslation('widgets')
     const isQualifierBetOn = useSelector(IsQualifierBetOn)
     const isQualifierBettable = isQualifierBetOn && isKnockout
-    
+
     const isTiedGame = !getWinnerSide(home.score, away.score)
-    const hasFullScore = (typeof home.fullScore === 'number') && (typeof away.fullScore === 'number')
+    const hasFullScore =
+        typeof home.fullScore === 'number' && typeof away.fullScore === 'number'
     const showFullScore = isQualifierBettable && isTiedGame && hasFullScore
     return (
-        <div className={`LB-MatchResult ${isAutoBet ? 'GameBetsTable-autoBet' : ''}`}>
-            {!!title && (
-                <div className='MatchResult-title'>
-                    {title}
-                </div>
-            )}
-            <div className='MatchResult-content'>
-                <div className='MatchResult-side'>
+        <div
+            className={`LB-MatchResult ${
+                isAutoBet ? 'GameBetsTable-autoBet' : ''
+            }`}
+        >
+            {!!title && <div className="MatchResult-title">{title}</div>}
+            <div className="MatchResult-content">
+                <div className="MatchResult-side">
                     <div className={'relative'}>
                         <TeamFlag size={32} team={home.team} />
                         {homeRole && <SpecialRoleBadge role={homeRole} />}
                     </div>
-                    <div className={'MatchResult-row'}>
-                        {home.score}
-                    </div>
+                    <div className={'MatchResult-row'}>{home.score}</div>
                     {showFullScore && (
                         <div className={'MatchResult-row'}>
                             ({home.fullScore})
                         </div>
                     )}
                     {isQualifierBettable && qualifier === WinnerSide.Home && (
-                        <div className={'MatchResult-row'}>
-                            ✌️
-                        </div>
+                        <div className={'MatchResult-row'}>✌️</div>
                     )}
                 </div>
-                <div className='MatchResult-delimiter'>
-                    <div>
-                        -
-                    </div>
-                    {isQualifierBettable && (<>
-                        {(!!qualifier || showFullScore) && (
-                            <div className={'MatchResult-row'}>
-                                90'
-                            </div>
-                        )}
-                        {showFullScore && (
-                            <div className={'MatchResult-row'}>
-                                120'
-                            </div>
-                        )}
-                        {!!qualifier && (
-                            <div className={'MatchResult-row'}>
-                                {t('matchResult.qualifier')}
-                            </div>
-                        )}
-                    </>)}
+                <div className="MatchResult-delimiter">
+                    <div>-</div>
+                    {isQualifierBettable && (
+                        <>
+                            {(!!qualifier || showFullScore) && (
+                                <div className={'MatchResult-row'}>90'</div>
+                            )}
+                            {showFullScore && (
+                                <div className={'MatchResult-row'}>120'</div>
+                            )}
+                            {!!qualifier && (
+                                <div className={'MatchResult-row'}>
+                                    {t('matchResult.qualifier')}
+                                </div>
+                            )}
+                        </>
+                    )}
                 </div>
-                <div className='MatchResult-side'>
-                    <TeamFlag size={32} team={away.team} />
-                    {awayRole && <SpecialRoleBadge role={awayRole} />}
-                    <div className={'MatchResult-row'}>
-                        {away.score}
+                <div className="MatchResult-side">
+                    <div className={'relative'}>
+                        <TeamFlag size={32} team={away.team} />
+                        {awayRole && <SpecialRoleBadge role={awayRole} />}
                     </div>
+                    <div className={'MatchResult-row'}>{away.score}</div>
                     {showFullScore && (
                         <div className={'MatchResult-row'}>
                             ({away.fullScore})
                         </div>
                     )}
                     {isQualifierBettable && qualifier === WinnerSide.Away && (
-                        <div className={'MatchResult-row'}>
-                            ✌️
-                        </div>
+                        <div className={'MatchResult-row'}>✌️</div>
                     )}
                 </div>
                 {isAutoBet && (
-                    <div className={cn('absolute left-1/2 -translate-x-1/2', {
-                        'bottom-2':!isQualifierBettable,
-                        'top-6 scale-75':isQualifierBettable
-                    })}>
+                    <div
+                        className={cn('absolute left-1/2 -translate-x-1/2', {
+                            'bottom-2': !isQualifierBettable,
+                            'top-6 scale-75': isQualifierBettable,
+                        })}
+                    >
                         <AutoBetBadge />
                     </div>
                 )}
